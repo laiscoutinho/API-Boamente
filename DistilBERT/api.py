5f0089e211929f538e028cd5f1bfe8664269ff65
laisdev <laiscc575@gmail.com>
2025-01-13 00:44:02 -0300

    Adding dashboard function

--- DistilBERT/api.py
+++ DistilBERT/api.py
@@ -1,134 +1,128 @@
 import asyncio
 import re
-import httpx
+# import httpx
 from typing import Dict
 from unidecode import unidecode
 from string import punctuation
+
 
 from fastapi import Depends, FastAPI
 from pydantic import BaseModel, Field
 from .classifier import BERTClassifier, get_bert
 
+
 # import requests
 # from starlette.requests import Request
 # from starlette.routing import request_response
 
+
 app = FastAPI()
 
+
 class ClassificationRequest(BaseModel):
-    text: str
-    identificador: str
-    datetime: str
+   text: str
+   identificador: str
+   datetime: str
+
 
 class ClassificationResponse(BaseModel):
-    probabilities: Dict[str, float]
-    sentiment: str
-    confidence: float
+   probabilities: Dict[str, float]
+   sentiment: str
+   confidence: float
+
 
 def preProText(text):
-    text = text.lower()
-    text = re.sub('@[^\s]+', '', text)
-    text = unidecode(text)
-    text = re.sub('<[^<]+?>', '', text)
-    text = ''.join(c for c in text if not c.isdigit())
-    text = re.sub(r'https?://\S+|www\.\S+', '', text)
-    text = ''.join(c for c in text if c not in punctuation)
-    return text
+   text = text.lower()
+   text = re.sub('@[^\s]+', '', text)
+   text = unidecode(text)
+   text = re.sub('<[^<]+?>', '', text)
+   text = ''.join(c for c in text if not c.isdigit())
+   text = re.sub(r'https?://\S+|www\.\S+', '', text)
+   text = ''.join(c for c in text if c not in punctuation)
+   return text
+
 
 def verTermos(text):
-    termos = [
-        "suicida", "suicidio", "me matar", "meu bilhete suicida",
-        "minha carta suicida", "acabar com a minha vida", "nunca acordar",
-        "não consigo continuar", "não vale a pena viver", "pronto para pular",
-        "dormir para sempre", "quero morrer", "estar morto",
-        "melhor sem mim", "vou me matar", "plano de suicídio", 
-        "cansado de viver", "morrer sozinho"
-    ]
-    return any(term in text for term in termos)
+   termos = [
+       "suicida", "suicidio", "me matar", "meu bilhete suicida",
+       "minha carta suicida", "acabar com a minha vida", "nunca acordar",
+       "não consigo continuar", "não vale a pena viver", "pronto para pular",
+       "dormir para sempre", "quero morrer", "estar morto",
+       "melhor sem mim", "vou me matar", "plano de suicídio",
+       "cansado de viver", "morrer sozinho", "vida", "morte", "morrer",
+       "sozinho", "sozinha", "solidão", "solidao", "tristeza", "triste", "depressão",
+       "depressao", "depressivo", "depressiva",
+   ]
+   termos_encontrados = [term for term in termos if term in text]
+   termos_na_classificacao = len(termos_encontrados)
+   termos_totais = len(termos)
 
-# POST
+
+   return termos_na_classificacao, termos_totais, termos_encontrados
+   # vai retornar qualquer texto que esteja contido na lista de termos
+   # return any(term in text for term in termos)
+
+
+
+
+# Função comum para classificação de texto
+def classify_text_logic(text, model):
+   texto = preProText(text)
+   termos_na_classificacao, termos_totais, termos_encontrados = verTermos(texto)
+
+
+   if termos_na_classificacao > 0:
+       try:
+           sentiment, confidence, probabilities = model.predict(texto)
+           probabilidade = round(float(confidence), 5)
+       except Exception as e:
+           raise RuntimeError(f"Erro ao realizar a predição: {e}")
+   else:
+       probabilidade = 0.0
+       sentiment = "Neutral"
+       probabilities = {}
+
+
+   return sentiment, probabilidade, probabilities, termos_na_classificacao, termos_totais, termos_encontrados
+
+
+# Endpoint POST para classificação
 @app.post("/classifica", response_model=ClassificationResponse)
 async def classifica(rqt: ClassificationRequest, model: BERTClassifier = Depends(get_bert)):
-    texto = preProText(rqt.text)
-    identificador = rqt.identificador
-    datetime = rqt.datetime
+   sentiment, probabilidade, probabilities, termos_na_classificacao, termos_totais, termos_encontrados = classify_text_logic(rqt.text, model)
 
-    url = "http://127.0.0.1:8000/classifica"
 
-    if verTermos(texto):
-        try:
-            sentiment, confidence, probabilities = model.predict(texto)
-            probabilidade = round(float(confidence), 5)
-            possibilidade = int(sentiment)
-        except Exception as e:
-            raise RuntimeError(f"Erro ao realizar a predição: {e}")
+   return ClassificationResponse(
+       sentiment=sentiment,
+       confidence=probabilidade,
+       probabilities={str(k): v for k, v in probabilities.items()}
+   )
 
-    else:
-        probabilidade = 0.0
-        possibilidade = 0
-        sentiment, probabilities = "Neutral", {}
 
-    payload = {
-        #'token': token,
-        'text': texto,
-        'identificador': identificador,
-        'probabilidade': probabilidade, 
-        'possibilidade': possibilidade,
-        'datetime': datetime
-    }
-
-    """
-    try:
-        async with httpx.AsyncClient(timeout=httpx.Timeout(90.0, read=90.0)) as client:
-            resposta = await client.post(url, json=payload)
-            resposta.raise_for_status()
-
-        print("Conectado com SUCESSO")
-    except httpx.HTTPStatusError as e:  # Específico para erros HTTP
-        print(f"Erro HTTP: {e.response.status_code}")
-        print(f"Detalhes do erro: {e.response.text}")
-        raise RuntimeError(f"Erro ao conectar ao servidor remoto: {str(e)}")
-    except httpx.RequestError as e:  # Erros relacionados à conexão
-        print(f"Erro de requisição: {e}")
-        raise RuntimeError(f"Erro ao conectar ao servidor remoto: {str(e)}")
-    """
-
-    return ClassificationResponse(
-    sentiment=str(sentiment),  # Convertendo para string
-    confidence=probabilidade,
-    probabilities={str(k): v for k, v in probabilities.items()}
-    )
-
-# GET
+# Endpoint GET para classificação
 @app.get("/", response_model=ClassificationResponse)
 async def root(text: str = "", model: BERTClassifier = Depends(get_bert)):
-    if not text:
-        return ClassificationResponse(
-            sentiment="Neutral",
-            confidence=0.0,
-            probabilities={}
-        )
+   if not text:
+       return ClassificationResponse(
+           sentiment="Neutral",
+           confidence=0.0,
+           probabilities={}
+       )
 
-    texto = preProText(text)
 
-    if verTermos(texto):
-        try:
-            sentiment, confidence, probabilities = model.predict(texto)
-            probabilidade = round(float(confidence), 5)
-            probabilities = int(sentiment)
-        except Exception as e:
-            raise RuntimeError(f"Erro ao realizar a predição: {e}")
-    else:
-        probabilidade = 0.0
-        probabilities = 0
-        sentiment, probabilities = "Neutral", {}
+   sentiment, probabilidade, probabilities, termos_na_classificacao, termos_totais, termos_encontrados = classify_text_logic(text, model)
 
-    return ClassificationResponse(
-        sentiment=sentiment,
-        confidence=probabilidade,
-        probabilities=probabilities
-    )
+
+   return ClassificationResponse(
+       sentiment=sentiment,
+       confidence=probabilidade,
+       probabilities=probabilities
+   )
+
 
 @app.get("/favicon.ico")
 async def favicon():
-    return {"message": "Favicon não configurado."}
+   return {"message": "Favicon não configurado."}
+
+
+
---
+++ DistilBERT/dashboard.py
@@ -0,0 +1,107 @@
+import asyncio
+import re
+# import httpx
+from typing import Dict
+from unidecode import unidecode
+from string import punctuation
+
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel, Field
+from .classifier import BERTClassifier, get_bert
+
+# import requests
+# from starlette.requests import Request
+# from starlette.routing import request_response
+
+app = FastAPI()
+
+class ClassificationRequest(BaseModel):
+    text: str
+    identificador: str
+    datetime: str
+
+class ClassificationResponse(BaseModel):
+    probabilities: Dict[str, float]
+    sentiment: str
+    confidence: float
+
+def preProText(text):
+    text = text.lower()
+    text = re.sub('@[^\s]+', '', text)
+    text = unidecode(text)
+    text = re.sub('<[^<]+?>', '', text)
+    text = ''.join(c for c in text if not c.isdigit())
+    text = re.sub(r'https?://\S+|www\.\S+', '', text)
+    text = ''.join(c for c in text if c not in punctuation)
+    return text
+
+def verTermos(text):
+    termos = [
+        "suicida", "suicidio", "me matar", "meu bilhete suicida",
+        "minha carta suicida", "acabar com a minha vida", "nunca acordar",
+        "não consigo continuar", "não vale a pena viver", "pronto para pular",
+        "dormir para sempre", "quero morrer", "estar morto",
+        "melhor sem mim", "vou me matar", "plano de suicídio", 
+        "cansado de viver", "morrer sozinho", "vida", "morte", "morrer",
+        "sozinho", "sozinha", "solidão", "solidao", "tristeza", "triste", "depressão", 
+        "depressao", "depressivo", "depressiva",
+    ]
+    termos_encontrados = [term for term in termos if term in text]
+    termos_na_classificacao = len(termos_encontrados)
+    termos_totais = len(termos)
+
+    return termos_na_classificacao, termos_totais, termos_encontrados
+    # vai retornar qualquer texto que esteja contido na lista de termos
+    # return any(term in text for term in termos)
+
+
+# Função comum para classificação de texto
+def classify_text_logic(text, model):
+    texto = preProText(text)
+    termos_na_classificacao, termos_totais, termos_encontrados = verTermos(texto)
+
+    if termos_na_classificacao > 0:
+        try:
+            sentiment, confidence, probabilities = model.predict(texto)
+            probabilidade = round(float(confidence), 5)
+        except Exception as e:
+            raise RuntimeError(f"Erro ao realizar a predição: {e}")
+    else:
+        probabilidade = 0.0
+        sentiment = "Neutral"
+        probabilities = {}
+
+    return sentiment, probabilidade, probabilities, termos_na_classificacao, termos_totais, termos_encontrados
+
+# Endpoint POST para classificação
+@app.post("/classifica", response_model=ClassificationResponse)
+async def classifica(rqt: ClassificationRequest, model: BERTClassifier = Depends(get_bert)):
+    sentiment, probabilidade, probabilities, termos_na_classificacao, termos_totais, termos_encontrados = classify_text_logic(rqt.text, model)
+
+    return ClassificationResponse(
+        sentiment=sentiment,
+        confidence=probabilidade,
+        probabilities={str(k): v for k, v in probabilities.items()}
+    )
+
+# Endpoint GET para classificação
+@app.get("/", response_model=ClassificationResponse)
+async def root(text: str = "", model: BERTClassifier = Depends(get_bert)):
+    if not text:
+        return ClassificationResponse(
+            sentiment="Neutral",
+            confidence=0.0,
+            probabilities={}
+        )
+
+    sentiment, probabilidade, probabilities, termos_na_classificacao, termos_totais, termos_encontrados = classify_text_logic(text, model)
+
+    return ClassificationResponse(
+        sentiment=sentiment,
+        confidence=probabilidade,
+        probabilities=probabilities
+    )
+
+@app.get("/favicon.ico")
+async def favicon():
+    return {"message": "Favicon não configurado."}
